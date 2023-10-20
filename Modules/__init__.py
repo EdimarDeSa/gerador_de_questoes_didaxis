f1feaf3129e73c2c935e74cc06539ddf368cfd99
Edimar de Sá <edimarfreitas95@gmail.com>
2023-10-20 01:19:25 -0300

    Ajustes
    Ajustes de inicialização

--- Modules/__init__.py
+++ Modules/__init__.py
@@ -1,1 +1,4 @@
-__version__ = '3.2.1'
+class MetaData:
+    __version__ = '3.2.1'
+    __author__ = 'Edimar Freitas de Sá'
+    __annotations__ = 'edimarfreitas95@gmail.com'
--- main.py
+++ main.py
@@ -1,253 +1,107 @@
-import re
-from os.path import basename
-from pathlib import Path
-from tkinter.filedialog import asksaveasfilename
-from tkinter.messagebox import showerror, showinfo, showwarning, askyesnocancel, askyesno
+from tkinter import Event
+from tkinter.messagebox import askyesnocancel
 
-from customtkinter import *
+from customtkinter import CTk
 
-from Modules.constants import *
+from Modules.funcoes import altera_aparencia, altera_escala, altera_cor_padrao
+from Modules.janelas import *
 from Modules.arquivos import Arquivos
+from Modules.configuration_manager import ConfigurationManager
 from Modules.imagens import Imagens
-from Modules.perfil import Perfil
-from Modules.configuracoes import *
-from Modules.quadro_de_questoes.quadro_de_questoes import QuadroDeQuestoes
-from Modules.painel_de_configuracoes import PainelDeConfiguracoes
 from Modules.corretor_ortografico import CorretorOrtografico
-from Modules.models.questao import ModeloQuestao
-from Modules.models.caixa_de_texto import CaixaDeTexto
-from Modules.atualizacao import Atualizacao
+from Modules.models.globalvars import VariaveisGlobais
+from Modules.questions_manager import QuestionsManager
 
 
 # noinspection PyAttributeOutsideInit
 class Main(CTk):
-    __version__ = 3.1
-    __author__ = 'Edimar Freitas de Sá'
-    __annotations__ = 'edimarfreitas95@gmail.com'
+    def __init__(self):
+        super().__init__()
 
-    def exportar(self):
-        if not self.caminho_arquivo:
-            self.caminho_arquivo = self.arquivos.caminho_salvar_para_salvar('Exportar para')
+        self.arquivos = Arquivos()
+        self.cnf_manager = ConfigurationManager(self.arquivos)
+        self.imagens = Imagens(self.arquivos.base_dir)
+        self.questions_manager = QuestionsManager()
 
-        retorno = self.arquivos.exportar(self.caminho_arquivo, self.questions_board.lista_de_questoes())
-        if not retorno:
-            showerror('Não foi possível exportar', 'Para exportar é necessário que o arqivo esteja fechado.'
-                                                   '\nConfirme que nenhum arquivo xlsx com mesmo nome esteja aberto.')
-        self.exportado = True
-        showinfo('Exportado', 'O banco de dados foi criado com sucesso!')
-        self.reseta_geral()
+        self.configura_ui_master()
+        self.configura_variaveis()
+        self.configura_ui()
+        # self.configura_binds()
 
-    def abrir(self):
-        if self.contador_de_opcoes:
-            resposta = askyesno(
-                'Questão em edição',
-                'Você tem questões em edição, deseja salva-la antes de abrir um novo arquivo?'
+        # self.after(500, self.verifica_atualizacao)
+
+        self.protocol('WM_DELETE_WINDOW', self.evento_de_fechamento_da_tela)
+
+        self.mainloop()
+
+    def configura_ui_master(self):
+        largura, altura = 1500, 750
+        pos_x = (self.winfo_screenwidth() - largura) // 2
+        pos_y = (self.winfo_screenheight() - altura) // 2 - 35
+        self.geometry(f'{largura}x{altura}+{pos_x}+{pos_y}')
+        self.resizable(False, False)
+        self.set_titulo('Editor de questões')
+        self.configura_aparencia()
+
+    def configura_aparencia(self):
+        altera_aparencia(self.cnf_manager.aparencia_do_sistema)
+        altera_cor_padrao(self.cnf_manager.cor_padrao)
+        altera_escala(self.cnf_manager.escala_do_sistema)
+
+    def configura_variaveis(self):
+        self.gvar = VariaveisGlobais(self.arquivos, self.cnf_manager, self.imagens)
+
+        self.gvar.corretor_ortografico = CorretorOrtografico(self.cnf_manager.PERSONAL_DICT_FILE,
+                                                             self.cnf_manager.add_palavra)
+        self.gvar.exit = self.evento_de_fechamento_da_tela
+        self.gvar.atualiza_titulo = self.set_titulo
+
+    def configura_ui(self):
+        JanelaQuantidadeDeQuestoes(self, self.gvar).place(relx=0.01, rely=0.02, relwidth=0.08, relheight=0.19)
+        # JanelaParametrosDaQuestao(self, self.gvar).place(relx=0.1, relwidth=0.395, rely=0.02, relheight=0.19)
+        # JanelaEnunciadoDaQuestao(self, self.gvar).place(relx=0.01, rely=0.23, relwidth=0.485, relheight=0.19)
+        # JanelaOpcoesDaQuestao(self, self.gvar).place(relx=0.01, rely=0.44, relwidth=0.485, relheight=0.46)
+        # JanelaDeQuestoes(self, self.gvar).place(relx=0.505, rely=0.02, relwidth=0.485, relheight=0.96)
+        # JanelaDeBotoes(self, self.gvar).place(relx=0.01, rely=0.92, relwidth=0.485, relheight=0.06)
+
+    def set_titulo(self, texto: str = 'Editor de questões'):
+        self.title(texto)
+
+    def evento_de_fechamento_da_tela(self):
+        if not self.gvar.exportado:
+            resposta = askyesnocancel(
+                'Salvamento pendente',
+                'Uma ou mais questões estão em edição e não foram exportadas.\n'
+                'Deseja exportar as alterações antes de sair?'
             )
-            if resposta:
-                self.salvar_como()
+            if resposta is None:
+                return
+            elif resposta:
+                self.gvar.exportar()
+        exit(0)
 
-        self.reseta_question_board()
-        self.exportado = True
-        self.reseta_informacoes()
-
-        self.caminho_arquivo = self.arquivos.buscar_arquivo_para_abrir()
-        if not self.caminho_arquivo:
-            return
-        questoes = self.arquivos.carrega_banco_de_dados(self.caminho_arquivo)
-
-        for questao in questoes:
-            self.questions_board.adiciona_questao(questao)
-
-        self.set_quantidade_de_questoes()
-        self.set_titulo()
-
-    def salvar_como(self):
-        self.caminho_arquivo = self.arquivos.caminho_salvar_para_salvar('Salvar como')
-        self.exportar()
-        self.set_titulo()
-
-    def seleciona_caminho(self, titulo: str):
-        self.caminho_arquivo = asksaveasfilename(confirmoverwrite=True, defaultextension=EXTENSIONS,
-                                                 filetypes=FILETYPES, initialdir=self.local, title=titulo)
-
-    def altera_unidade_padrao(self):
-        nova_unidade = self.var_nova_unidade_padrao.get()
-        self.unidade.set(nova_unidade)
-        self.salva_informacao('perfil', 'unidade_padrao', nova_unidade)
-
-    def altera_alternativa(self, e=None):
-        quantidade_de_questoes = self.contador_de_opcoes
-        for indice in range(quantidade_de_questoes):
-            self.rm_alternativa()
-        for indice in range(quantidade_de_questoes):
-            self.add_alternativa()
-        self.organiza_ordem_tabulacao()
-
-    def set_titulo(self):
-        self.title(f'Gerador de questões - {basename(self.caminho_arquivo)}' if self.caminho_arquivo else 'Gerador de questões')
-
-    def set_quantidade_de_questoes(self):
-        self.var_quantidade_de_questoes.set(f'Número de questões: '
-                                            f'{self.questions_board.quantidade_de_questoes_registradas()}')
-
-    def organiza_ordem_tabulacao(self):
-        ordem_widgets = [
-            self.codigo_do_curso,
-            self.tempo,
-            self.peso,
-            self.pergunta,
-        ]
-
-        for widget in ordem_widgets:
-            widget.lift()
-
-    def get_opcao_bt(self, indice) -> [CTkRadioButton, CTkCheckBox]:
-        def show_rd_bt() -> CTkRadioButton:
-            rd_bt = getattr(self, f'rd_button_opcao{indice}')
-            return rd_bt
-
-        def show_ck_bt() -> CTkCheckBox:
-            ck_bt = getattr(self, f'ck_button_opcao{indice}')
-            return ck_bt
-
-        def show_dissert():
-            pass
-
-        tipos_btn = {
-            ME: show_rd_bt,
-            MEN: show_ck_bt,
-            VF: show_ck_bt,
-            D: show_dissert
-        }
-        return tipos_btn[self.get_tipo]()
-
-    def show_opcao(self, indice):
-        pos_y = self.calcula_posicao_y(indice) + 0.04
-        pos_x = 0.95
-
-        self.get_opcao_bt(indice).place(relx=pos_x, rely=pos_y)
-
-    def editar_questao(self, questao: ModeloQuestao):
-        self.reseta_informacoes()
-        self.questao_em_edicao = questao
-        self.codigo_do_curso.insert(0, questao.subcategoria)
-        self.tempo.insert(0, questao.tempo)
-        self.tipo.set(questao.tipo)
-        self.dificuldade.set(questao.dificuldade)
-        self.peso.insert(0, questao.peso)
-        self.pergunta.insert(0.0, questao.pergunta)
-        for alternativa, correta in questao.alternativas:
-            self.add_alternativa(alternativa)
-            if correta:
-                opcao_bt = self.get_opcao_bt(self.contador_de_opcoes - 1)
-                opcao_bt.select()
-
-    @property
-    def get_opcoes(self) -> list[tuple[str, bool]]:
-        def verifica_correta(botao, indice):
-            if self.get_tipo == ME:
-                return self.var_opcao_correta_radio_bt.get() == indice
-            return botao.get()
-
-        opcoes = list()
-        for indice in range(self.contador_de_opcoes):
-            texto: CaixaDeTexto = self.txt_opcao(indice)
-            alternativa = texto.get_texto_completo()
-            correta = verifica_correta(self.get_opcao_bt(indice), indice)
-            opcoes.append((alternativa, correta))
-        return opcoes
-
-    @property
-    def get_tipo(self):
-        return self.tipo.get()
-
-    @property
-    def get_unidade(self):
-        return self.unidade.get()
-
-    @property
-    def get_codigo(self):
-        return self.codigo_do_curso.get()
-
-    @property
-    def get_tempo(self):
-        tempo = self.tempo.get()
-        if not tempo:
-            tempo = '00:00:00'
-        return tempo
-
-    @property
-    def get_dificuldade(self):
-        return self.dificuldade.get()
-
-    @property
-    def get_peso(self):
-        peso = self.peso.get()
-        if not peso:
-            peso = 1
-        return peso
-
-    @property
-    def get_pergunta(self):
-        return self.pergunta.get_texto_completo()
-
-    @staticmethod
-    def calcula_posicao_y(multiplo):
-        return (0.18 * multiplo) + 0.1
-
-    def reseta_informacoes(self):
-        self.unidade.set(self.configuracao_unidade_padrao)
-        self.codigo_do_curso.delete(0, 'end')
-        self.tempo.delete(0, 'end')
-        self.peso.delete(0, 'end')
-        self.var_opcao_correta_radio_bt.set(0)
-
-        if self.var_apagar_enunciado:
-            self.pergunta.delete(0.0, 'end')
-
-        for indice in range(5):
-            self.txt_opcao(indice).delete(0.0, 'end')
-            self.rm_alternativa()
-
-        self.pergunta.focus()
-        self.set_quantidade_de_questoes()
-
-    def salvar_edicao(self):
-        self.questao_em_edicao.campo_unidade = self.get_unidade
-        self.questao_em_edicao.subcategoria = self.get_codigo
-        self.questao_em_edicao.tempo = self.get_tempo
-        self.questao_em_edicao.tipo = self.get_tipo
-        self.questao_em_edicao.dificuldade = self.get_dificuldade
-        self.questao_em_edicao.peso = self.get_peso
-        self.questao_em_edicao.pergunta = self.get_pergunta
-        self.questao_em_edicao.alternativas = self.get_opcoes
-
-        self.questions_board.salvar_edicao_de_questao(self.questao_em_edicao)
-
-        self.questao_em_edicao = None
-        self.reseta_informacoes()
-
-    def init_binds(self):
-        def ctrl_events(e):
+    def configura_binds(self):
+        def ctrl_events(e: Event):
             key = str(e.keysym).lower()
 
-            def seleciona_tipo(indice: int):
-                self.tipo.set(self.tipos[indice])
-                self.altera_alternativa()
+            # def seleciona_tipo(indice: int):
+            #     self.gvar.tipo.set(self.gvar.configs.tipos[indice])
+            #     self.gvar.altera_alternativa()
 
             def seleciona_dificuldade(indice: int):
-                self.dificuldade.set(self.dificuldades[indice - 4])
+                self.gvar.dificuldade.set(self.gvar.configs.dificuldades[indice - 4])
 
             events = {
-                'e': self.exportar,
-                's': self.salvar,
-                'o': self.abrir,
-                'equal': self.add_alternativa,
-                'plus': self.add_alternativa,
-                'minus': self.rm_alternativa,
-                '1': seleciona_tipo,
-                '2': seleciona_tipo,
-                '3': seleciona_tipo,
+                # 'e': self.exportar,
+                # 's': self.salvar,
+                'o': abrir,
+                'equal': self.gvar.add_alternativa,
+                'plus': self.gvar.add_alternativa,
+                'minus': self.gvar.rm_alternativa,
+                # 'backspace': self.limpa_tab
+                # '1': seleciona_tipo,
+                # '2': seleciona_tipo,
+                # '3': seleciona_tipo,
                 '4': seleciona_dificuldade,
                 '5': seleciona_dificuldade,
                 '6': seleciona_dificuldade,
@@ -262,80 +116,11 @@ class Main(CTk):
         def key_events(e):
             key = e.keysym
             events = {
-                'F1': self.abre_atalhos,
-                'F4': self.evento_de_fechamento_da_tela,
-                'F12': self.salvar_como,
+                # 'F1': self.abre_atalhos,
+                # 'F12': self.gvar.arquivos.salvar_como,
             }
             if key in events.keys():
                 return events[key]()
 
-        self.bind('<Control-KeyRelease>', ctrl_events)
+        self.bind('<Control-Key>', ctrl_events)
         self.bind('<KeyRelease>', key_events)
-        self.protocol(self.protocol()[0], self.evento_de_fechamento_da_tela)
-
-    def evento_de_fechamento_da_tela(self):
-        if not self.exportado:
-            tittle = 'Salvamento pendente'
-            information = 'Uma ou mais questões estão em edição e não foram exportadas.\n' \
-                          'Deseja exportar as alterações antes de sair?'
-            resposta = askyesnocancel(tittle, information)
-            if resposta is None:  # Se resposta for cancelar
-                return
-            elif resposta:  # Se resposta for sim
-                self.exportar()
-        # Se resposta for não ou se arquivo já estiver sido exportado
-        self.quit()
-
-    def abre_atalhos(self):
-        self.abre_menu_configuracoes()
-        self.painel_de_configuracoes.abre_ajuda()
-
-    def reseta_question_board(self):
-        self.questions_board.destroy()
-        self.create_questions_frame()
-
-    def reseta_geral(self):
-        self.reseta_informacoes()
-        self.reseta_question_board()
-        self.caminho_arquivo = None
-        self.questao_em_edicao = None
-        self.set_titulo()
-        self.set_quantidade_de_questoes()
-
-    def verifica_digito(self, keysym):
-        pass
-
-    def verifica_atualizacao(self):
-        if not getattr(self, 'atualizador', False):
-            self.atualizador = Atualizacao(self.__version__, self.local)
-
-        resposta = None
-        if self.atualizador.atualizacao_disponivel:
-            resposta = askyesno(
-                'Nova atualização disponível',
-                f'O programa está atualmente na versão {self.__version__}.\n'
-                f'A versão disponível é a {self.atualizador.versao_recente}.\n'
-                f'Deseja atualizar agora?'
-            )
-
-        if resposta:
-            self.atualizador.atualiza()
-            self.after(1200, self.evento_de_fechamento_da_tela)
-
-    def verifica_texto_opcoes(self):
-        if self.get_tipo == D:
-            return False
-        for indice in range(self.contador_de_opcoes):
-            opcao: CaixaDeTexto = self.txt_opcao(indice)
-            texto = opcao.get_texto_completo()
-            if not texto:
-                return True
-        return False
-
-    def txt_opcao(self, indice):
-        return getattr(self, f'txt_opcao{indice}')
-
-
-if __name__ == '__main__':
-    app = Main()
-    app.mainloop()
