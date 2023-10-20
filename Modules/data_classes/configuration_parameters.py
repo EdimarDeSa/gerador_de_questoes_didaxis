2a9112be4c18cfd82b4418d10d89793c3b1ff549
Edimar de Sá <edimarfreitas95@gmail.com>
2023-10-19 23:20:09 -0300

    Update configuration_parameters.py
    Adicionada função de atualização de atributo e serialização da informação no arquivo json

--- Modules/data_classes/configuration_parameters.py
+++ Modules/data_classes/configuration_parameters.py
@@ -1,4 +1,8 @@
 from dataclasses import dataclass, field
+from pathlib import Path
+from typing import Literal
+
+from ..funcoes import FileSerializer
 
 
 __all__ = ['Configuracoes']
@@ -6,12 +10,10 @@ __all__ = ['Configuracoes']
 
 @dataclass
 class Configuracoes:
-    cor_da_borda: str = 'darkgreen'
-    cor_de_fundo: str = 'lightgreen'
     fonte: str = 'Arial'
     tamanho_texto: int = 8
     tamanho_titulo: int = 10
-    fonte_estilo: str = 'bold'
+    fonte_estilo: Literal['bold', 'normal'] = 'bold'
     tipos: list[str] = field(default_factory=lambda: [
         'Multipla escolha 1 correta', 'Multipla escolha n corretas', 'Verdadeiro ou falso'
     ])
@@ -20,3 +22,8 @@ class Configuracoes:
         'Negócios', 'Redes', 'Segurança eletrônica', 'Solar', 'Soluções', 'Varejo', 'Verticais'
     ])
     dificuldades: list[str] = field(default_factory=lambda: ['Fácil', 'Médio', 'Difícil'])
+
+    def atualiza_atributo(self, *, path: Path, key: str, value: any):
+        setattr(self, key, value)
+        FileSerializer.save_json(path, self.__dict__)
+
