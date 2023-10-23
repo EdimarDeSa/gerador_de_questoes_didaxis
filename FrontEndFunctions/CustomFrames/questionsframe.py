65286b54304c904b3c32197a3b39c67be0ffcd9d
Edimar de Sá <edimarfreitas95@gmail.com>
2023-10-23 01:17:25 -0300

    Update questionsframe.py
    Alterado tipo da classe para simplificar o código

--- FrontEndFunctions/CustomFrames/questionsframe.py
+++ FrontEndFunctions/CustomFrames/questionsframe.py
@@ -1,38 +1,30 @@
 from customtkinter import CTk, CTkFrame, CTkLabel, CTkScrollableFrame, CTkImage, BOTH, X
 
-from ..Hints import ConfigsHint, ImageHint, RowHint
+from ..Hints import ConfigsHint, ImageHint
 from ..Constants import VERDE
 
 
-class QuestionsFrame(CTkFrame):
+class QuestionsFrame(CTkScrollableFrame):
     def __init__(
-            self, master: CTk, label_configs: ConfigsHint, img_edit: ImageHint, img_delete: ImageHint,
-            frame_questoes: CTkScrollableFrame, **kwargs
+            self, master: CTk, label_configs: ConfigsHint,
+            img_edit: ImageHint, img_delete: ImageHint, **kwargs
     ):
         super().__init__(master, **kwargs)
 
         self.label_configs = label_configs
         self.img_edit = img_edit
         self.img_delete = img_delete
-        self.frame_questoes = frame_questoes
 
-        self.frame_questoes = CTkScrollableFrame(self)
-        self.frame_questoes.pack(fill=BOTH, expand=True)
+        self._init_header(self)
 
-        self._row_dict: RowHint = dict()
-        self._zebrar: bool = True
-        self._linha_atual: int = 0
-
-        self._init_header()
-
-    def _init_header(self) -> None:
+    def _init_header(self, master: CTkScrollableFrame) -> None:
         fg_label_configs = self.label_configs.copy()
         fg_label_configs['fg_color'] = VERDE
 
         bg_label_configs = self.label_configs.copy()
         bg_label_configs['bg_color'] = VERDE
 
-        frame = CTkFrame(self.frame_questoes, fg_color=VERDE, height=45)
+        frame = CTkFrame(master, fg_color=VERDE, height=45)
         frame.pack(expand=True, fill=X)
 
         CTkLabel(frame, text='Enunciado', **fg_label_configs).place(relx=0.01, relwidth=0.8, relheight=1)
