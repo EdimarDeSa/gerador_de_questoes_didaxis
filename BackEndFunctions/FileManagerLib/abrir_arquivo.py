76d96e66cbe9d193abb8af6f81518dabe292fb36
Edimar de Sá <edimarfreitas95@gmail.com>
2023-10-22 23:46:25 -0300

    Update abrir_arquivo.py
    Correção de funcionamento

--- BackEndFunctions/FileManagerLib/abrir_arquivo.py
+++ BackEndFunctions/FileManagerLib/abrir_arquivo.py
@@ -1,27 +1,12 @@
 from pathlib import Path
-from tkinter.filedialog import askopenfilename
 from pandas import ExcelFile, read_excel, DataFrame, Series
 from pandas.errors import InvalidColumnName
 
-from BackEndFunctions.Constants import (ENGINE, CABECALHO_DIDAXIS, CABECALHO_PERGUNTA, CABECALHO_ALTERNATIVAS, CORRETA,
-                                        V, EXTENSION, FILETYPES)
+from BackEndFunctions.Constants import ENGINE, CABECALHO_DIDAXIS, CABECALHO_PERGUNTA, CABECALHO_ALTERNATIVAS, CORRETA, V
 
 
 class AbrirArquivo:
-    def __init__(self):
-        self._abrir()
-
-    @staticmethod
-    def get_desktop_path() -> Path:
-        """
-        Retorna o caminho do diretório de desktop do usuário.
-
-        Returns the user's desktop directory path.
-        """
-        return Path.home() / "Desktop"
-
-    @classmethod
-    def _open_excel(cls, path: Path) -> DataFrame | Exception | InvalidColumnName:
+    def _open_excel(self, path: Path) -> DataFrame | Exception | InvalidColumnName:
         """
         Abre um arquivo Excel e retorna um DataFrame ou uma exceção em caso de erro.
 
@@ -36,8 +21,6 @@ class AbrirArquivo:
         except Exception as err:
             return Exception(err)
 
-        if not cls._verify_columns(df.columns):
-            raise InvalidColumnName(f'O arquivo Excel não tem as colunas necessárias: {CABECALHO_DIDAXIS}')
         return df
 
     @staticmethod
@@ -50,6 +33,7 @@ class AbrirArquivo:
         :param df: O DataFrame a ser dividido.
         :return: Uma lista_serial de DataFrames.
         """
+
         grup = df.groupby('PERGUNTA')
         return [grupo for _, grupo in grup]
 
@@ -76,9 +60,8 @@ class AbrirArquivo:
         :return: Um dicionário com chaves em letras minúsculas.
         """
         return {key.lower(): value for key, value in to_normalize.items()}
-
-    @classmethod
-    def _normalize_choices(cls, choices: list) -> list[tuple[str, bool]]:
+    
+    def _normalize_choices(self, choices: list) -> list[tuple[str, bool]]:
         """
         Normaliza as escolhas das questões.
 
@@ -87,10 +70,10 @@ class AbrirArquivo:
         :param choices: Uma lista_serial de escolhas de questão.
         :return: Uma lista_serial de tuplas (alternativa: str, correto: bool).
         """
-        return [(alternativa, cls._verifies_correct(correta)) for alternativa, correta in choices]
 
-    @classmethod
-    def _split_question_choices(cls, question: DataFrame) -> dict[str, list[tuple[str, bool]]]:
+        return [(alternativa, self._verifies_correct(correta)) for alternativa, correta in choices]
+
+    def _split_question_choices(self, question: DataFrame) -> list[tuple[str, bool]]:
         """
         Divide as alternativas da questão.
 
@@ -99,12 +82,12 @@ class AbrirArquivo:
         :param question: O DataFrame de uma questão.
         :return: Um dicionário com as alternativas da questão em formato de lista_serial de tuplas com uma string e um booleano.
         """
-        choices = question[CABECALHO_ALTERNATIVAS].values.tolist()
-        normalized_choices = cls._normalize_choices(choices)
-        return dict(alternativas=normalized_choices)
 
-    @classmethod
-    def _serialize_questions(cls, question_list: list[DataFrame]) -> list[dict]:
+        choices = question[CABECALHO_ALTERNATIVAS].values.tolist()
+
+        return self._normalize_choices(choices)
+
+    def _serialize_questions(self, question_list: list[DataFrame]) -> list[dict]:
         """
         Serializa as questões em uma lista_serial de dicionários.
 
@@ -113,13 +96,17 @@ class AbrirArquivo:
         :param question_list: Uma lista_serial de DataFrames de questões.
         :return: Uma lista_serial de dicionários serializados.
         """
+
         serials = []
         for question in question_list:
-            serial_dict = cls._split_question_parameters(question)
-            alternativas = cls._split_question_choices(question)
-            serial_dict.update(alternativas)
-            serial_dict = cls._normalize_keys(serial_dict)
-            serials.append(serial_dict)
+            serial_dict = self._split_question_parameters(question)
+            alternativas = self._split_question_choices(question)
+
+            serial_dict['alternativas'] = alternativas
+            normalized_serial_dict = self._normalize_keys(serial_dict)
+
+            serials.append(normalized_serial_dict)
+
         return serials
 
     @staticmethod
@@ -130,7 +117,7 @@ class AbrirArquivo:
         return correct in [CORRETA, V]
 
     @staticmethod
-    def _verify_columns(columns: Series) -> bool:
+    def _verify_columns(columns: list) -> None:
         """
         Verifica se o DataFrame tem as mesmas colunas do CABECALHO_DIDAXIS.
         Retorna True se as colunas coincidirem, False caso contrário.
@@ -138,14 +125,12 @@ class AbrirArquivo:
         Checks if the DataFrame has the same columns as CABECALHO_DIDAXIS.
         Returns True if the columns match, False otherwise.
         """
-        colunas_do_dataframe = columns.to_list()
-        colunas_do_dataframe.sort()
-        columns = CABECALHO_DIDAXIS.copy()
-        columns.sort()
-        return colunas_do_dataframe == columns
 
-    @classmethod
-    def _abrir(cls):
+        if columns != CABECALHO_DIDAXIS:
+            raise InvalidColumnName(f'O arquivo não contem as colunas necessárias:\n'
+                                    f'{CABECALHO_DIDAXIS}')
+
+    def open(self, path: str):
         """
         Função principal para abrir e serializar questões de um arquivo Excel.
 
@@ -153,16 +138,16 @@ class AbrirArquivo:
 
         :return: Uma lista_serial de questões serializadas ou uma exceção.
         """
-        path = askopenfilename(defaultextension=EXTENSION, filetypes=FILETYPES, initialdir=cls.get_desktop_path())
 
         if not path:
             raise FileNotFoundError('Não foi possível encontrar o arquivo.')
 
-        path = Path(path).resolve()
-        try:
-            data_frame = cls._open_excel(path)
-        except InvalidColumnName as err:
-            raise InvalidColumnName(err)
+        abs_path = Path(path).resolve()
 
-        question_list = cls._split_questions(data_frame)
-        cls.serialized_questions = cls._serialize_questions(question_list)
+        data_frame = self._open_excel(abs_path)
+
+        self._verify_columns(data_frame.columns.to_list())
+
+        question_list = self._split_questions(data_frame)
+
+        return self._serialize_questions(question_list)
