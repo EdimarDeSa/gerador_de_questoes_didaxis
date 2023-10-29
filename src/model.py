2127471c5701b28da5776a5dd4cc5b8f7ea70114
Edimar de Sá <edimarfreitas95@gmail.com>
2023-10-29 02:39:41 -0300

    Update model.py
    Implementation of a contratct
    methods organization, grouping by context
    implmentation of:
    Create question, read question, update question, validade question_data

--- src/model.py
+++ src/model.py
@@ -1,77 +1,159 @@
 from pathlib import Path
-from PIL import Image
-from dataclasses import dataclass, field
+from itertools import groupby
 
-from src.Hints.hints import SysImgHint, Dict
-from src.Constants import CATEGORYLIST, QUESTIONTYPELIST, DIFFICULTLIST
+from icecream import ic
 
-
-
-
-@dataclass(kw_only=True)
-class ImageManager:
-    icons_dir: Path
-    image_paths: Dict[str, str]
-
-    def __post_init__(self):
-        self._images = {name: self._abre_imagem(path) for name, path in self.image_paths.items()}
-
-    def _abre_imagem(self, nome_imagem: str) -> Image:
-        caminho_imagem = self.icons_dir / nome_imagem
-        return Image.open(caminho_imagem)
-
-    def get_images(self) -> SysImgHint:
-        return self._images
+from src.Constants import QUESTIOHEADER, QUESTIONTYPELIST, CATEGORYLIST, DIFFICULTLIST, D
+from src.contracts.model import ModelContract
+from src.contracts.serializer import Serializer
+from src.Hints.hints import SysImgHint, QuestionDataHint, ImageModelHint, UserSetHint, Any, Optional, Iterable, GroupedQuestionDBHint, ListDBHint
+from src.Models.imagemodel import ImageModel
+from src.Models.questionsdb import QuestionsDB
+from src.Models.usermodel import UserModel
+from src.Models.Serializers.json_serializer import JsonSerializer
+from src.Models.Serializers.binary_serializer import BinarySerializer
+from src.Models.Serializers.xlsxserializer import XLSXSerializer
 
 
-@dataclass
-class UserManager:
-    erase_statement: bool = False
-    auto_export: bool = False
-    user_default_category: str = field(default='')
-    user_color_theme: str = field(default='green')
-    user_scaling: str = field(default='100%')
-    user_appearance_mode: str = field(default='system')
-    titles_font_settings: dict = field(default_factory=dict)
-    default_font_settings: dict = field(default_factory=dict)
-    category_options: list = field(default_factory=list)
-    question_type_list: list = field(default_factory=list)
-    difficulty_list: list = field(default_factory=list)
-
-    def __iter__(self):
-        return iter(self.__dict__.items())
-
-    def updatesetting(self, attribute: str, value: any) -> None:
-        setattr(self, attribute, value)
-
-
-class Model:
+class Model(ModelContract):
+    # ------ Initialization ------ #
     def __init__(self):
-        self.base_dir = Path(__file__).resolve().parent.parent
+        self._base_dir = Path(__file__).resolve().parent.parent
+        self._base_filename: Optional[str] = None
+        self.__db_connection = QuestionsDB()
+    # ------  ------ #
 
-        icons_dir = self.base_dir / 'icons'
-        self._img_manager = ImageManager(icons_dir=icons_dir, image_paths={
-            'configuracoes_light_mode': 'configuracoes_light_mode.png',
-            'configuracoes_dark_mode': 'configuracoes_dark_mode.png',
-            'eraser_light_mode': 'eraser_light_mode.png',
-            'eraser_dark_mode': 'eraser_dark_mode.png',
-            'edit_light_mode': 'edit_light_mode.png',
-            'edit_dark_mode': 'edit_dark_mode.png',
-        })
-        self.system_images: SysImgHint = self._img_manager.get_images()
+    # ------ Question Handling ------ #
+    def create_new_question(self, question_data: QuestionDataHint) -> int:
+        self._validate_question_data(question_data)
 
-        configs_dir = self.base_dir / 'configs'
-        self._user_manager = UserManager(
-            titles_font_settings={'font': ('Roboto', 15, 'bold')},
-            default_font_settings={'font': ('Roboto', 12)},
-            category_options=CATEGORYLIST,
-            question_type_list=QUESTIONTYPELIST,
-            difficulty_list=DIFFICULTLIST,
-            user_default_category='Comunicação'
-        )
-        self.user_settings = dict(self._user_manager).copy()
+        control = self.__db_connection.create_question(question_data)
 
-    def save_user_settings(self, param: str, value: any) -> None:
-        # print('update settings', param, value)
-        if param in dict(self._user_manager).keys():
-            self._user_manager.updatesetting(param, value)
+        if not control: raise ConnectionError('Não foi possível conectar se banco de dados')
+
+        return control
+
+    def read_question(self, control: int) -> QuestionDataHint:
+        question = self.__db_connection.read_question(control)
+
+        if not question: raise KeyError(f'Questão com controle: {control} inexistente')
+
+        return question
+
+    def update_question(self, question_data: QuestionDataHint) -> None:
+        self._validate_question_data(question_data)
+
+        self.__db_connection.update_question(question_data)
+
+    def delete_question(self, control: int) -> None:
+        self.__db_connection.delete_question(control)
+
+    def flush_questions(self) -> None:
+        self.__db_connection.flush_questions()
+
+    def _get_all_questions(self) -> Iterable[QuestionDataHint]:
+        return self.__db_connection.select_all_questions()
+    # ------  ------ #
+
+    # ------ Questions xlsx Handler ------ #
+    def create_question_xlsx(self, questions_data: Iterable[Iterable[QuestionDataHint]], file_path: Path) -> None:
+        all_questions = self._get_all_questions()
+        self._select_serializer()
+        pass
+
+    def read_question_xlsx(self, filename: Path) -> GroupedQuestionDBHint:
+        data: Iterable = self._read_file(filename)
+
+        lines: ListDBHint = [dict(zip(QUESTIOHEADER, line)) for id_, line in enumerate(data) if id_]
+
+        lista_dicionarios_ordenada = sorted(lines, key=lambda x: x['pergunta'])
+
+        return {chave: list(grupo) for chave, grupo in groupby(lista_dicionarios_ordenada, key=lambda x: x['pergunta'])}
+    # ------  ------ #
+
+    # ------ System Images ------ #
+    def read_system_images(self, image_names: ImageModelHint) -> SysImgHint:
+        icons_dir = self._base_dir / 'icons'
+        image_paths = {img_key: icons_dir / img_name for img_key, img_name in image_names.items()}
+
+        self._img_manager = ImageModel(image_paths)
+        return self._img_manager.get_images()
+    # ------  ------ #
+
+    # ------ User Settings ------ #
+    def create_user_settings(self, configs_dir: Path) -> UserSetHint:
+        self._user_settings = UserModel()
+
+        self._save_file(configs_dir, dict(self._user_settings))
+
+        return dict(self._user_settings)
+
+    def read_user_settings(self, configs_dir: Path) -> UserSetHint:
+        user_data = self._read_file(configs_dir)
+
+        self._user_settings = UserModel(**user_data)
+        return dict(self._user_settings)
+
+    def update_user_settings(self, param: str, value: any, file_path: Path) -> None:
+        if param in dict(self._user_settings).keys():
+            self._user_settings.updatesetting(param, value)
+            self._save_file(file_path, dict(self._user_settings))
+            return
+        raise KeyError(f'User setting "{param}" does not exist')
+    # ------  ------ #
+
+    # ------ File Handling and Serialization ------ #
+    def create_path(self, filename: str) -> Path:
+        return self._base_dir / filename
+
+    def _read_file(self, file_path: Path) -> Any:
+        serializer = self._select_serializer(file_path)
+        return serializer.import_from_path(file_path)
+
+    def _save_file(self, file_path: Path, data: Any):
+        serializer = self._select_serializer(file_path)
+        serializer.export_to_path(file_path, data)
+
+    @staticmethod
+    def _select_serializer(filename: Path) -> Serializer:
+        match filename.suffix.lower():
+            case '.json':
+                return JsonSerializer()
+            case '.bin':
+                return BinarySerializer()
+            case '.xlsx':
+                return XLSXSerializer()
+            case '.csv':
+                pass
+            case '.txt':
+                pass
+            case _:
+                raise ValueError('Invalid extension!')
+
+    def get_base_dir(self) -> Path:
+        return self._base_dir
+
+    def get_base_filename(self) -> str:
+        return self._base_filename
+    # ------  ------ #
+
+    def _validate_question_data(self, data: QuestionDataHint) -> None:
+        if not data: raise SyntaxError('Faltam informações para uma questão válida')
+
+        # if self.read_question(data['controle']): KeyError('Questão já existente')
+
+        if data['categoria'] not in CATEGORYLIST: raise KeyError(f'Categoria: {data['categoria']} deve ser um dos {CATEGORYLIST}')
+
+        if len(data['tempo'].split(':')) != 3: raise KeyError(f'Tempo: {data['tempo']} com formato inválido deve ser 00:00:00')
+
+        if data['tipo'] not in QUESTIONTYPELIST: raise KeyError(f'Tipo: {data['tipo']} deve ser um dos {QUESTIONTYPELIST}')
+
+        if data['dificuldade'] not in DIFFICULTLIST: raise KeyError(f'Dificuldade: {data['dificuldade']} deve ser um dos {DIFFICULTLIST}')
+
+        if not isinstance(data['peso'], int): raise ValueError(f'Peso {data['peso']} deve ser do tipo int')
+
+        if not data['pergunta']: raise ValueError(f'Pergunta {data['pergunta']} não pode ser None')
+
+        if data['tipo'] == D: return
+
+        if not any([bool(choice) for choice, _ in data['alternativas']]): raise ValueError(f'Alternativa {data['alternativas']} não pode conter opçõa em branco')
