a630fde998e130ca6976f0d48dbd775a85202875
Edimar de Sá <edimarfreitas95@gmail.com>
2023-10-29 23:07:28 -0300

    Export to path implemetation

--- src/Models/Serializers/xlsxserializer.py
+++ src/Models/Serializers/xlsxserializer.py
@@ -5,11 +5,18 @@ from openpyxl.worksheet.worksheet import Worksheet
 
 from src.contracts.serializer import Serializer
 from src.Hints.hints import Iterable
+from src.Constants import QUESTIOHEADER
 
 
 class XLSXSerializer(Serializer):
     def export_to_path(self, file_path: Path, data: Iterable) -> None:
-        pass
+        wb = Workbook()
+        ws: Worksheet = wb.active
+        ws.append([h.upper() for h in QUESTIOHEADER])
+        for line_data in data:
+            ws.append([line_data[header] for header in QUESTIOHEADER])
+
+        wb.save(file_path)
 
     def import_from_path(self, file_path: Path) -> Iterable:
         wb: Workbook = load_workbook(file_path, read_only=True, data_only=True)
