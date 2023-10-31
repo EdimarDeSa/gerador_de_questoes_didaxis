feabd479eb1f2f77c417aecc84f06fec1d1d5adc
Edimar de Sá <edimarfreitas95@gmail.com>
2023-10-30 23:34:07 -0300

    Create widgetssettingsmodel.py

---
+++ src/DataModels/widgetssettingsmodel.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+
+from src.Hints import MenuSettingsHint
+
+
+@dataclass(frozen=True, kw_only=True)
+class WidgetsSettingsModel:
+    label_settings: MenuSettingsHint = field(default_factory=dict)
+    entry_settings: MenuSettingsHint = field(default_factory=dict)
+    list_settings: MenuSettingsHint = field(default_factory=dict)
+    button_title_settings: MenuSettingsHint = field(default_factory=dict)
+    button_default_settings: MenuSettingsHint = field(default_factory=dict)
+    text_settings: MenuSettingsHint = field(default_factory=dict)
+    scrollable_label_settings: MenuSettingsHint = field(default_factory=dict)
