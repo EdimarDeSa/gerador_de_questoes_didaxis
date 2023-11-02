d78db0d77c7a0427591976021cbfa00faecfb1c7
Edimar de Sá <edimarfreitas95@gmail.com>
2023-11-01 21:29:02 -0300

    Update usermodel.py
    Optimizations

--- src/DataModels/usermodel.py
+++ src/DataModels/usermodel.py
@@ -1,20 +1,19 @@
 from dataclasses import dataclass, field
 
-from src.Constants import CATEGORYLIST, DIFFICULTLIST, QUESTIONTYPELIST
 from src.Hints import List
 
 
 @dataclass(frozen=True)
 class UserModel:
-    auto_export: bool = field(default=0)
+    auto_export: bool = False
     category_options: List[str] = field(default_factory=list)
-    default_font_size: int = field(default=12)
+    default_font_size: int = 12
     difficulty_list: List[str] = field(default_factory=list)
-    erase_statement: bool = field(default=0)
-    font_family: str = field(default='Roboto')
+    erase_statement: bool = False
+    font_family: str = 'Roboto'
     question_type_list: List[str] = field(default_factory=list)
-    title_font_size: int = field(default=15)
-    user_appearance_mode: str = field(default='system')
-    user_color_theme: str = field(default='green')
-    user_default_category: str = field(default='')
-    user_scaling: str = field(default='100%')
+    title_font_size: int = 15
+    user_appearance_mode: str = 'system'
+    user_color_theme: str = 'green'
+    user_default_category: str = ''
+    user_scaling: str = '100%'
