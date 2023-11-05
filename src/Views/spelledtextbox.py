c31e7923d60340f351841dd7b9ef836c040587b8
Edimar de Sá <edimarfreitas95@gmail.com>
2023-11-05 02:10:53 -0300

    Update spelledtextbox.py
    Responsabilitires chaged.
    Implemented new spellcheck support

--- src/Views/spelledtextbox.py
+++ src/Views/spelledtextbox.py
@@ -1,120 +1,37 @@
 import dataclasses
-from tkinter import Event
+from tkinter import Event, Menu
+import tkinter as tk
 from tkinter.messagebox import showwarning
-from typing import Callable
+from typing import Callable, NamedTuple, Dict
 
 from customtkinter import CTkTextbox
 
-from src.Constants import MAXIMO_DE_CARACTERES, RED
+from src.Constants import MAXIMO_DE_CARACTERES, RED, ADD
 from src.Contracts.spelledtextbox import SpelledTextBoxContract
 
 
 @dataclasses.dataclass(frozen=True)
-class Vector:
-    x: float
-    y: float
-
-
-@dataclasses.dataclass(frozen=True)
-class Suggestions:
-    vector: Vector
+class TagSettings:
+    name: str
     suggestions: list[str] = dataclasses.field(default_factory=list)
+    index1: float = None
+    index2: float = None
 
 
 class SpelledTextBox(CTkTextbox, SpelledTextBoxContract):
     def __init__(self, master, input_speller_queue: Callable, **kwargs):
         super().__init__(master, border_color=RED, **kwargs)
 
-        self.bind('<KeyRelease>', self._verifica_quantidade_digitos, add='+')
+        self.bind('<KeyRelease>', self._check_max_caracters, add='+')
         self.bind('<KeyRelease>', self._corretor_ortografico, add='+')
 
-        self.palavras_com_suggests: dict = {}
+        self.palavras_com_suggests: Dict[str, TagSettings] = {}
         self.input_speller_queue = input_speller_queue
+        self.cmd_add_new_word = None
 
         self.alerta_exibido = False
 
-    def _corretor_ortografico(self, event: Event):
-        if event.keysym.isalpha():
-            self.input_speller_queue(self)
-
-    def get_suggestions(self, word: str) -> set[str]:
-        return self.palavras_com_suggests.get(word).get('suggests')
-
-    def register_suggestions(self, word: str, suggestions: set[str]) -> None:
-        if not suggestions: return
-
-        x = self.search(word, 0.0, nocase=True)
-        y = self.index(f"{x} + {len(word)}c")
-        vector = Vector(x, y)
-
-        self.get(vector.x, vector.y)
-
-        tag_name = f'{word}_{vector.x}'
-
-        print('register_suggestions')
-
-        suggestions_list = (
-            suggestions if len(suggestions) < 4 else list(suggestions)[:5]
-        )
-
-        self.tag_add(tag_name, vector.x, vector.y)
-
-        self.tag_config(tag_name, underline=1, foreground='red')
-
-        self.palavras_com_suggests[tag_name] = {Suggestions(
-            suggestions=suggestions_list,
-            vector=vector
-        )}
-        print(self.palavras_com_suggests)
-
-
-
-    def registra_posicao_inicial(self, palavra, start_index):
-        self.palavras_com_suggests.setdefault(palavra, {})[
-            'posicao_inicial'
-        ] = start_index
-
-    def get_posicao_inicial(self, palavra):
-        return self.palavras_com_suggests.get(palavra, {}).get(
-            'posicao_inicial'
-        )
-
-    def registra_posicao_final(self, palavra, end_index):
-        self.palavras_com_suggests.setdefault(palavra, {})[
-            'posicao_final'
-        ] = end_index
-
-    def get_posicao_final(self, palavra):
-        return self.palavras_com_suggests.get(palavra, {}).get(
-            'posicao_final'
-        )
-
-    def cria_tag(self, palavra, comando) -> None:
-        tag_name = f'corretor_ortografico_{self.get_posicao_inicial(palavra)}'
-        self.palavras_com_suggests.setdefault(palavra, {})[
-            'nome_da_tag'
-        ] = tag_name
-
-        self.tag_add(
-            tag_name,
-            self.get_posicao_inicial(palavra),
-            self.get_posicao_final(palavra),
-        )
-        self.tag_config(tag_name, underline=True, underlinefg=RED)
-        self.tag_bind(
-            tag_name, '<3>', lambda event, p=palavra: comando(event, p)
-        )
-
-    def get_nome_da_tag(self, palavra: str) -> str:
-        return self.palavras_com_suggests.get(palavra, {}).get('nome_da_tag')
-
-    def remove_correcao_pela_tag(self, nome_da_tag) -> None:
-        self.tag_delete(nome_da_tag)
-        for palavra, dados in list(self.palavras_com_suggests.items()):
-            if dados.get('nome_da_tag') == nome_da_tag:
-                self.palavras_com_suggests.pop(palavra)
-
-    def _verifica_quantidade_digitos(self, _) -> None:
+    def _check_max_caracters(self, _) -> None:
         total_digitos = len(self.get(1.0, 'end-1c'))
         if total_digitos > MAXIMO_DE_CARACTERES:
             self.tag_add('tamanho', f'1.{MAXIMO_DE_CARACTERES}', 'end-1c')
@@ -133,3 +50,98 @@ class SpelledTextBox(CTkTextbox, SpelledTextBoxContract):
         else:
             self.configure(border_width=0)
             self.alerta_exibido = False
+
+    def _corretor_ortografico(self, event: Event):
+        if event.keysym.isalpha():
+            self.input_speller_queue(self)
+
+    def get_suggestions(self, word: str) -> list[str]:
+        return self.palavras_com_suggests.get(word).suggestions
+
+    def register_new_word_cmd(self, cmd_add_new_word: Callable) -> None:
+        self.cmd_add_new_word = cmd_add_new_word
+
+    def register_suggestions(self, word: str, suggestions: set[str]) -> None:
+        if not suggestions: return
+
+        tag_name = word
+
+        if tag_name in self.palavras_com_suggests:
+            self.remove_tag(tag_name)
+
+        index1 = self.search(word, 0.0, nocase=True)
+        index2 = self.index(f"{index1} + {len(word)}c")
+
+        filtered_suggestions = (
+            suggestions if len(suggestions) < 4 else list(suggestions)[:5]
+        )
+
+        tag_settings = TagSettings(tag_name, filtered_suggestions, index1, index2)
+
+        self.palavras_com_suggests[word] = tag_settings
+
+        self.register_tag(tag_name)
+
+    def remove_tag(self, tag_name: str) -> None:
+        self.tag_delete(tag_name)
+        for word, tag_settings in list(self.palavras_com_suggests.items()):
+            if tag_settings.name == tag_name:
+                self.palavras_com_suggests.pop(word)
+                return
+
+    def register_tag(self, word: str) -> None:
+        tag_settings = self.palavras_com_suggests.get(word)
+
+        self.tag_add(tag_settings.name, tag_settings.index1, tag_settings.index2)
+
+        self.tag_config(tag_settings.name, underline=True, underlinefg='red')
+
+        self.tag_bind(
+            tag_settings.name,
+            '<3>',
+            lambda event, w=word: self.show_correction_menu(event, w),
+            add='+'
+        )
+
+    def show_correction_menu(self,event: Event, word: str) -> None:
+        pop_up_menu = Menu(self, tearoff=False, font='Arial 12')
+        for correction in self.palavras_com_suggests.get(word).suggestions:
+            if correction == 'Sem sugestões':
+                pop_up_menu.add_command(
+                    label=correction, command=self._nothing_to_do
+                )
+                break
+
+            pop_up_menu.add_command(
+                label=correction,
+                command=lambda c=correction, w=word: self.__apply_correction(c, w),
+            )
+
+        pop_up_menu.add_separator()
+
+        pop_up_menu.add_command(
+            label=ADD,
+            command=lambda c=ADD, w=word: self.__apply_correction(c, w),
+        )
+        pop_up_menu.post(event.x_root, event.y_root)
+
+    def __apply_correction(self, correction: str, word: str) -> None:
+        tag_settings = self.palavras_com_suggests.pop(word)
+
+        self.remove_tag(tag_settings.name)
+        self.focus_set()
+
+        if correction == ADD:
+            self.cmd_add_new_word(word)
+            return
+
+        self.delete(tag_settings.index1, tag_settings.index2)
+        self.insert(tag_settings.index1, correction)
+
+    def remove_all_tags(self):
+        for tag_settings in list(self.palavras_com_suggests.values()):
+            self.remove_tag(tag_settings.name)
+
+    @staticmethod
+    def _nothing_to_do() -> None:
+        pass
