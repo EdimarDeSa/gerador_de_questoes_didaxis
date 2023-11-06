05375bf2b73c457f3e40fae51bb1d630e14fdcf8
Edimar de Sá <edimarfreitas95@gmail.com>
2023-11-05 23:16:38 -0300

    Update pyspellchecker.py
    Testing cache to speed up verification.
    Optimized the tokenized set to prevent occasional set errors.
    Fixed a bug that caused word frequency exports to crash.

--- src/Speller/pyspellchecker.py
+++ src/Speller/pyspellchecker.py
@@ -1,5 +1,5 @@
-import pathlib
 from pathlib import Path
+from functools import lru_cache, cache
 
 from spellchecker import SpellChecker
 
@@ -10,19 +10,23 @@ class PySpellChecker(SpellChecker, SpellerContract):
     def __init__(self, local_dictionary: Path):
         self.local_dictionary = str(local_dictionary)
 
-        super().__init__(local_dictionary=self.local_dictionary, case_sensitive=True)
+        super().__init__(
+            local_dictionary=self.local_dictionary, case_sensitive=True
+        )
 
+    @lru_cache
     def tokenize_words(self, text: str) -> set[str]:
-        return set(self.split_words(text))
+        return set([word for word in self.split_words(text) if not word.isupper()])
 
     def check_spelling(self, words_list: set[str]) -> set[str]:
         return self.unknown(words_list)
 
+    @cache
     def suggest_corrections(self, word: str) -> set[str]:
         return self.candidates(word)
 
     def export_word_usage(self) -> None:
-        self.export(self.local_dictionary, gzipped=True)
+        self.export(self.local_dictionary, gzipped=False)
 
     def add_new_word(self, word: str) -> None:
         self._word_frequency.add(word)
