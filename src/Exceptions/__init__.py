168ec3a0f835567fe1cd7bba64102319b8dba0a9
Edimar de Sá <edimarfreitas95@gmail.com>
2023-11-29 09:13:52 -0300

    Update Exceptions file
    Splited exceptions to better read.

--- src/Exceptions/__init__.py
+++ src/Exceptions/__init__.py
@@ -1,18 +1,2 @@
-class ExceptionBaseClass(Exception):
-    def __init__(self, message: str):
-        self.message = message
-
-
-class QuestionMatchError(Exception):
-    def __init__(self, controle: int):
-        self.message = f'A questão "<{controle}>" tem a mesma pergunta!'
-
-
-class BrokenFileError(Exception):
-    def __init__(self, message: str = None):
-        self.message = message
-
-
-class QuestionValidationError(Exception):
-    def __init__(self, message: str):
-        self.message = message
+from .questionvalidationerror import QuestionValidationError
+from .brokenfileerror import BrokenFileError
---
+++ src/Exceptions/brokenfileerror.py
@@ -0,0 +1,3 @@
+class BrokenFileError(Exception):
+    def __init__(self, message: str = None):
+        self.message = message
