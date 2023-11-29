00743988ebf1241d2f1149e0d493434c7dbbc57e
Edimar de Sá <edimarfreitas95@gmail.com>
2023-11-29 09:10:21 -0300

    Version checker update
    Updating Version checker

--- src/VersionChecker/versionchecker.py
+++ src/VersionChecker/versionchecker.py
@@ -1,26 +1,46 @@
+import subprocess
+import tomllib
+
 import requests
+
+from ..DataModels.versionmodel import VersionInfo
 
 
 class VersionChecker:
     def __init__(self, software_name: str):
-        self.id = None
-        self.name = software_name
-        self.pud_date = None
-        self.__version__ = None
+        self.base_url = (
+            f'https://efscode.com.br/atualizacoes/verifica_versao/{software_name}'
+        )
 
-        self.url = f'https://efscode.com.br/atualizacoes/verifica_versao/{self.name}'
+        self.version_infos = self.check_atual_version_info()
 
-        self._check_version()
+        self.new_version_available = False
 
-    def _check_version(self):
+    def check_version(self) -> None:
         try:
+            response = requests.get(self.base_url).json()
+            print(response)
 
-            response = requests.get(self.url).json()
+            new_version_info = VersionInfo(**response)
 
-            self.id = response['ID']
-            self.name = response['Nome']
-            self.pud_date = response['Publicação']
-            self.__version__ = response['Versão']
+            current_version, current_release, current_patch = self.version_infos.version.split('.')
+            new_version, new_release, new_patch = new_version_info.version.split('.')
+
+            if not any((
+                    current_version < new_version,
+                    current_release < new_release,
+                    current_patch < new_patch
+            )):
+                self.new_version_available = True
 
         except requests.ConnectionError:
             raise ConnectionError('Sem conexão com o servidor para atualizações.')
+
+    def update(self) -> None:
+        subprocess.call(['../SUT.exe', f'--name {self.version_infos.name}'], stdout=False, shell=True)
+
+    @staticmethod
+    def check_atual_version_info() -> VersionInfo:
+        with open('./pyproject.toml', 'rb') as toml_file:
+            infos = tomllib.load(toml_file)['tool']['poetry']
+            return VersionInfo(**infos)
--- src/Views/SetupViews/HelpFrame/versionframe.py
+++ src/Views/SetupViews/HelpFrame/versionframe.py
@@ -1,45 +1,28 @@
 from customtkinter import CTkButton, CTkFrame, CTkLabel, CTkToplevel
 
-from src.Hints.hints import MenuSettingsHint, Callable
+from src.Hints.hints import Callable, MenuSettingsHint
 
 
 class VersionFrame(CTkFrame):
-    __version__ = None
-
     def __init__(
-            self,
-            master: CTkToplevel,
-            label_configs: MenuSettingsHint,
-            version_verify: Callable,
-            **kwargs
+        self,
+        master: CTkToplevel,
+        label_configs: MenuSettingsHint,
+        get_version: Callable,
+        version: str,
+        **kwargs,
     ):
         super().__init__(master, **kwargs)
-
-        self.get_project_version()
-
-        self.version_verify = version_verify
 
         self.columnconfigure(0, weight=1)
         self.columnconfigure(1, weight=1)
         self.rowconfigure(0, weight=3)
 
         # TODO: Implementar atualização
-        CTkLabel(
-            self, text=f'Versão: {self.__version__}', **label_configs
-        ).grid(row=0, column=0)
-        CTkButton(self, text='Verificar atualização', command=self.check_verify).grid(
-            row=0, column=1
+        CTkLabel(self, text=f'Versão: {version}', **label_configs).grid(
+            row=0, column=0
         )
 
-    def check_verify(self):
-        new_version = self.version_verify()
-        print(new_version)
-
-    @classmethod
-    def get_project_version(cls):
-        import tomllib
-
-        with open('./pyproject.toml', 'rb') as file:
-            pyproject_data = tomllib.load(file)
-
-        cls.__version__ = pyproject_data['tool']['poetry']['version']
+        CTkButton(self, text='Verificar atualização', command=get_version).grid(
+            row=0, column=1
+        )
